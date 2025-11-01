08c6f28b762e11a5cdca454417396072de0202de
eesalor <197505674+eesalor@users.noreply.github.com>
2025-11-01 11:46:55 +0200

    Dependency injection to NHL stats

--- viikko1/nhl-statistics/src/statistics_service.py
+++ viikko1/nhl-statistics/src/statistics_service.py
@@ -1,10 +1,9 @@
 from player_reader import PlayerReader
 
 
-
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, reader):
+        self._reader = reader
 
         self._players = reader.get_players()
 
