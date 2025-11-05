c77fafa1652450f672cbb926745757547e756947
eesalor <197505674+eesalor@users.noreply.github.com>
2025-11-05 10:38:19 +0200

    Add NHL reader of Finnish players

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,26 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.nationality == "FIN":
+            players.append(player)
+
+    print("Players from FIN:")
+    print()
+
+    for player in players:
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
